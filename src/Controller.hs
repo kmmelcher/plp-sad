b92a278cd09075849cc7ae508b0a315885ba8f94
Pedro Adrian <pedro.martinez@ccc.ufcg.edu.br>
2022-02-23 17:25:50 -0300

    Resolving conflict with addAluno branch

--- src/Controller.hs
+++ src/Controller.hs
@@ -14,3 +14,14 @@ addProfessor = do
         let prof = Professor id nome disciplinas
         let profToString = show (prof)
         adicionaLinha "../database/Professores.txt" profToString
+
+createAluno :: IO()
+    createAluno = do
+        putStrLn "Insira o nome do aluno: "
+        nome <- getLine
+        putStrLn "Insira a matricula do aluno"
+        matricula <- readLn  
+        putStrLn "Insira as disciplinas do aluno"
+        disciplinas <- readLn 
+        let aluno = Aluno matricula nome disciplinas
+        adicionaLinha "alunos" $ show aluno
