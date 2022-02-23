84a732e885237a83a220b736a74d99ea53cdce50
Vinícius Azevedo <vinicius.azevedo@ccc.ufcg.edu.br>
2022-02-23 17:39:41 -0300

    Update Controller.hs

--- Src/Controller.hs
+++ Src/Controller.hs
@@ -1,6 +1,21 @@
 module Src.Controller where
     import Src.Util.TxtFunctions
     import Src.Model.Aluno
+    import Src.Model.Professor
+
+    addProfessor :: IO()
+    addProfessor = do
+            putStrLn "Insira o id do professor: "
+            idStr <- getLine
+            let id = read(idStr) :: Integer
+            putStrLn "Insira o nome do professor: "
+            nome <- getLine
+            putStrLn "Insira o nome das disciplinas do professor: "
+            listaDisciplinasStr <- getLine
+            let disciplinas = read(listaDisciplinasStr) :: [String]
+            let prof = Professor id nome disciplinas
+            let profToString = show (prof)
+            adicionaLinha "../database/Professores.txt" profToString
 
     createAluno :: IO()
     createAluno = do
