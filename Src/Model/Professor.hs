cc44cb96e6f7b3b404f5f9e6bf68660c03efdba9
Vinícius Azevedo <vinicius.azevedo@ccc.ufcg.edu.br>
2022-02-23 17:36:25 -0300

    Update Professor.hs

--- Src/Model/Professor.hs
+++ Src/Model/Professor.hs
@@ -1,7 +1,8 @@
 module Src.Model.Professor where
     import Src.Model.Disciplina (Disciplina)
+    import GHC.Read (Read)
     data Professor = Professor {
         id :: Integer,
         nome :: String,
         disciplinas:: [String]
-    }
+    } deriving (Show, Read)
