51d9e64d37b1a0585ea840ff749dfb3e6cb50b04
Vinícius Azevedo <vinicius.azevedo@ccc.ufcg.edu.br>
2022-02-24 23:19:05 -0300

    Criado diretório de controladores
    > Agora, existe um diretório (Módulo) de controladores para cada entidade relevante do sistema
    > Todos os possíveis problemas de imports já foram resolvidos, tanto na Main.hs quanto nos próprios controladores

---
+++ Src/Controller/ProfessorController.hs
@@ -0,0 +1,15 @@
+module Src.Controller.ProfessorController where
+    import Src.Model.Professor
+    import Src.Util.TxtFunctions
+
+    adicionaProfessor :: IO()
+    adicionaProfessor = do
+            putStrLn "Insira o nome do professor: "
+            nome <- getLine
+            putStrLn "Insira o nome das disciplinas do professor: "
+            listaDisciplinasStr <- getLine
+            let disciplinas = read(listaDisciplinasStr) :: [String]
+            id <- buscaNovoId "Professores"
+            let prof = Professor (read id :: Int) nome disciplinas
+            let profToString = show (prof)
+            adicionaLinha "Professores" profToString
