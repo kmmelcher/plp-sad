16ea95655bf2f98ee59a72df9017415f75595bbd
Vinícius Azevedo <vinicius.azevedo@ccc.ufcg.edu.br>
2022-02-27 16:58:38 -0300

    Adicionada funcionalidade de matricular aluno em disciplina
    > Ainda faltam testes quanto a esta funcionalidade!

--- Src/Controller/AlunoController.hs
+++ Src/Controller/AlunoController.hs
@@ -1,6 +1,7 @@
 module Src.Controller.AlunoController where
-    import Src.Model.Aluno
+    import Src.Model.Aluno as A
     import Src.Util.TxtFunctions
+    import Src.Controller.DisciplinaController as DC
     
     adicionaAluno :: IO()
     adicionaAluno = do
@@ -12,3 +13,13 @@ module Src.Controller.AlunoController where
         disciplinas <- readLn
         let aluno = Aluno matricula nome disciplinas
         adicionaLinha "alunos" $ show aluno
+    
+    matriculaAlunoEmDisciplina :: Aluno -> IO()
+    matriculaAlunoEmDisciplina aluno = do
+        putStrLn "Atualmente, estas são todas as disciplinas disponíveis para matrícula, exibidas em id, nome e sigla:\n\n"
+        exibeDisciplinas aluno
+        putStrLn "\nInforme a sigla da disciplina na qual deseja se matricular:"
+        sigla <- getLine 
+        let alunoAtualizado = Aluno (A.id aluno) (nome aluno) ([sigla] ++ disciplinas aluno)
+        atualizaLinhaById "Alunos" (show (A.id aluno)) (show aluno)
+        putStrLn "Matricula realizada com sucesso!"
