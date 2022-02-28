e905d7c9e56c043e19433c5bbe0a571f6fff457b
Filipe Ramalho <filpe.silva@ccc.ufcg.edu.br>
2022-02-28 19:20:15 -0300

    implementa a função de remover o ticket de um aluno

--- Src/Controller/AlunoController.hs
+++ Src/Controller/AlunoController.hs
@@ -1,7 +1,9 @@
 module Src.Controller.AlunoController where
     import Src.Model.Aluno
     import Src.Util.TxtFunctions
-    
+    import Src.Controller.ChatController
+    import Src.Model.Ticket
+
     adicionaAluno :: IO()
     adicionaAluno = do
         putStrLn "Insira o nome do aluno: "
@@ -13,3 +15,22 @@ module Src.Controller.AlunoController where
         let aluno = Aluno matricula nome disciplinas
         adicionaLinha "Alunos" $ show aluno
         putStrLn "Aluno cadastrado com sucesso.\n"
+
+    excluirTicket :: Int -> IO()
+    excluirTicket matAluno = do
+        putStrLn "Escolha entre os seus Tickets qual será excluido :"
+        mostraTickets matAluno
+        sel <- getLine
+        removeLinha "Tickets" sel
+
+    mostraTickets :: Int -> IO()
+    mostraTickets mat = do
+        ticketsIds <- pegaTicketsDoAluno mat
+        mostraTicketsRec ticketsIds
+
+    mostraTicketsRec :: [Int] -> IO()
+    mostraTicketsRec [] = print " "
+    mostraTicketsRec (head:tail) = do
+        ticketStr <- buscaObjetoById "Tickets" head
+        let ticket = read ticketStr :: Ticket
+        putStrLn $ show (Src.Model.Ticket.id ticket) ++ ": " ++ titulo ticket
