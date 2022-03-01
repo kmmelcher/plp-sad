0e3b7b540374509fecf37380fa5007048f810b03
kmmelcher <kilian.melcher@gmail.com>
2022-03-01 11:15:59 -0300

    Correcoes de monitor controller

--- Src/Controller/MonitorController.hs
+++ Src/Controller/MonitorController.hs
@@ -5,23 +5,20 @@ module Src.Controller.MonitorController where
     adicionaMonitor :: IO()
     adicionaMonitor = do
         id <- insereMatricula
-        if id == 0
+        disciplina <- insereDisciplina id
+        if disciplina == "VOLTAR"
             then putStrLn "Cadastro cancelado.\n"
-        else do 
-            disciplina <- insereDisciplina
-            if disciplina == "VOLTAR"
-                then putStrLn "Cadastro cancelado.\n"
-            else do
-                putStrLn "Insira os horários de atendimento do monitor"
-                horarios <- getLine
-                let monitor = Monitor id disciplina horarios
-                adicionaLinha "Monitores" $ show monitor
-                putStrLn "Monitor cadastrado com sucesso!\n"
+        else do
+            putStrLn "Insira os horários de atendimento do monitor"
+            horarios <- getLine
+            let monitor = Monitor id disciplina horarios
+            adicionaLinha "Monitores" $ show monitor
+            putStrLn "Monitor cadastrado com sucesso!\n"
 
     insereMatricula :: IO(Int)
     insereMatricula = do
         putStrLn "Insira a matricula do monitor"
-        id <- readLn
+        id <- readLn 
         alunoCadastrado <- checaExistenciaById "Alunos" id
         if alunoCadastrado || id == 0
             then return id
@@ -29,13 +26,16 @@ module Src.Controller.MonitorController where
             putStrLn "Aluno não cadastrado!\n(Digite 0 para voltar ao menu principal)\n"
             insereMatricula
 
-    insereDisciplina :: IO(String)
-    insereDisciplina = do
-        putStrLn "Insira a disciplina do monitor"
-        disciplina <- getLine
-        disciplinaCadastrada <- checaExistenciaByAtributo  "Disciplinas" "sigla" (adicionaAspas disciplina)
-        if disciplinaCadastrada || disciplina == "VOLTAR"
-            then return disciplina
-        else do
-            putStrLn "Disciplina não cadastrada!\n(Digite \"VOLTAR\" para voltar ao menu principal)\n"
-            insereDisciplina
+    insereDisciplina :: Int -> IO(String)
+    insereDisciplina id = do
+        if id == 0
+            then return "VOLTAR"
+        else do            
+            putStrLn "Insira a disciplina do monitor"
+            disciplina <- getLine
+            disciplinaCadastrada <- checaExistenciaByAtributo  "Disciplinas" "sigla" (adicionaAspas disciplina)
+            if disciplinaCadastrada || disciplina  == "VOLTAR"
+                then return disciplina
+            else do
+                putStrLn "Disciplina não cadastrada!\n(Digite \"VOLTAR\" para voltar ao menu principal)\n"
+                insereDisciplina id
