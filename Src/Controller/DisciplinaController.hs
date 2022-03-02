751ac56a27782dbada64910b0842aabf9f18317b
Filipe Ramalho <filipe.silva@ccc.ufcg.edu.br>
2022-03-02 14:17:57 -0300

    Tira sublinados azuis dos controllers

--- Src/Controller/DisciplinaController.hs
+++ Src/Controller/DisciplinaController.hs
@@ -2,8 +2,8 @@ module Src.Controller.DisciplinaController where
     import Src.Model.Disciplina as D
     import Src.Util.TxtFunctions
     import Src.Model.Aluno as A
-    
-    getDisciplina:: Int -> IO(Disciplina)
+
+    getDisciplina:: Int -> IO Disciplina
     getDisciplina id = do
         disciplinaToString <- getObjetoById "Disciplinas" id
         return (read disciplinaToString :: Disciplina)
@@ -12,24 +12,24 @@ module Src.Controller.DisciplinaController where
     exibeDisciplinasDisponiveis aluno = do
         disciplinas <- fileToStringArray "Disciplinas"
         exibeDisciplinasDisponiveisRecursivo disciplinas aluno
-    
+
     exibeDisciplinasDisponiveisRecursivo :: [String] -> Aluno  -> IO()
     exibeDisciplinasDisponiveisRecursivo [] _ = return ()
     exibeDisciplinasDisponiveisRecursivo (disciplinaAtual:disciplinasRestantes) aluno = do
         let disciplina = (read disciplinaAtual :: Disciplina)
-        if (show (D.sigla disciplina)) `elem` (disciplinas aluno)
+        if show (D.sigla disciplina) `elem` disciplinas aluno
             then exibeDisciplinasDisponiveisRecursivo disciplinasRestantes aluno else do
             putStrLn (show (D.id disciplina) ++ ") " ++ show (D.nome disciplina) ++ " - " ++ show (sigla disciplina))
             exibeDisciplinasDisponiveisRecursivo disciplinasRestantes aluno
-    
+
     getSiglas :: IO([String])
-    getSiglas = do 
+    getSiglas = do
         disciplinas <- fileToStringArray "Disciplinas"
         getSiglasRecursivo disciplinas []
-    
-    getSiglasRecursivo :: [String] -> [String] -> IO([String])
+
+    getSiglasRecursivo :: [String] -> [String] -> IO[String]
     getSiglasRecursivo [] ret = return ret
-    getSiglasRecursivo (disciplinaAtual:disciplinasRestantes) ret = do 
+    getSiglasRecursivo (disciplinaAtual:disciplinasRestantes) ret = do
         let disciplina = (read disciplinaAtual :: Disciplina)
-        getSiglasRecursivo disciplinasRestantes ((D.sigla disciplina) : ret)
+        getSiglasRecursivo disciplinasRestantes (D.sigla disciplina : ret)
         
