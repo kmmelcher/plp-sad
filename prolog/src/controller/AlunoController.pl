271a8042197e51600207cdb24e4faa545a66e992
AlefAdonis <alefadonis10@gmail.com>
2022-03-26 11:35:03 -0300

    Adicionando funcçao para vincular um aluno a uma disciplina

--- prolog/src/controller/AlunoController.pl
+++ prolog/src/controller/AlunoController.pl
@@ -3,3 +3,10 @@
 getAluno(Id, Aluno):-
     (checaExistencia("alunos", Id) -> getObjetoByID("alunos", Id, Aluno); 
     Aluno = "Inexistente").
+
+vinculaAlunoDisciplina(Id, Disciplina):-
+    getAluno(Id, Aluno),
+    DisciplinasAntigas = Aluno.disciplinas,
+    \+ member(Disciplina, DisciplinasAntigas),
+    append(DisciplinasAntigas, [Disciplina], DisciplinasNovas),
+    atualizaAtributoAluno(Id, "disciplinas", DisciplinasNovas).
