18eae229d0eaeb736993e03e42d62c22e297fdb2
Vinícius Azevedo <vinicius.azevedo@ccc.ufcg.edu.br>
2022-03-29 15:06:33 -0300

    siglas de disciplina em minusculo

--- prolog/src/controller/ChatController.pl
+++ prolog/src/controller/ChatController.pl
@@ -2,15 +2,18 @@
 
 :- use_module('../util/jsonFunctions.pl', [readJSON/2]).
 
-exibeTicketsDisciplina(SiglaDisciplina):-
+exibeTicketsDisciplina():-
+    writeln("Insira a disciplina:"),
+    read(SiglaDisciplina),
+    atom_string(SiglaDisciplina, SiglaString),
     readJSON("tickets", Tickets),
-    exibeTicketsDisciplinaRecursivo(Tickets, SiglaDisciplina).
+    exibeTicketsDisciplinaRecursivo(Tickets, SiglaString).
 
 exibeTicketsDisciplinaRecursivo([], _).
 
 exibeTicketsDisciplinaRecursivo([T|Ts], SiglaDisciplina):-
     T.disciplina = SiglaDisciplina,
     swritef(Out, "%w) %w (%w)", [T.id, T.titulo, T.status]), write(Out), nl,
-    exibeTicketsDisciplinaRecursivo(Ts, SiglaDisciplina)    
+    exibeTicketsDisciplinaRecursivo(Ts, SiglaDisciplina)
     ;
     exibeTicketsDisciplinaRecursivo(Ts, SiglaDisciplina).
