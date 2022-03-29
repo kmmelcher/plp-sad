eef76a507820a641a9ada53f554fe8cf5eea99b8
kmmelcher <kilian.melcher@gmail.com>
2022-03-29 10:46:19 -0300

    Add monitor controller

--- prolog/src/controller/AlunoController.pl
+++ prolog/src/controller/AlunoController.pl
@@ -1,8 +1,10 @@
+:- module('AlunoController', [getAluno/2]).
+
 :- include("../util/jsonFunctions.pl").
 
 getAluno(Id, Aluno):-
-    (checaExistencia("alunos", Id) -> getObjetoByID("alunos", Id, Aluno); 
-    Aluno = "Inexistente").
+    checaExistencia("alunos", Id),
+    getObjetoByID("alunos", Id, Aluno).
 
 vinculaAlunoDisciplina(Id, Disciplina):-
     getAluno(Id, Aluno),
--- prolog/src/util/jsonFunctions.pl
+++ prolog/src/util/jsonFunctions.pl
@@ -1,3 +1,5 @@
+:- module('jsonFunctions', [readJSON/2, checaExistencia/2, getObjetoByID/3]).
+
 :- use_module(library(http/json)).
 
 getIDs([], ListaIDs, Result):- append([0], ListaIDs, Result).
@@ -50,7 +52,8 @@ getObjetoByID(NomeArquivo, Id, Result):-
 
 checaExistencia(NomeArquivo, Id):-
     readJSON(NomeArquivo, File),
-    getObjetoRecursivamente(File, Id, Result),
+    atom_string(Id, IdString),
+    getObjetoRecursivamente(File, IdString, Result),
     Result \= "".
 
 %-------------------------- Funções de Alunos--------------------------%
